18324218524cc36a39a23224c02f3e8a3c5e14dc
Pratik Singh <singhpratik0001@gmail.com>
2023-06-30 00:36:45 +0530

    Whole Api Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Do barr jo api call hoti hai vo "React.StrictMode" ki wajah se hoti hai ye check krta hai response ko dono barr k aur vo response same hone chaiye
   <React.StrictMode>
     <App />
   </React.StrictMode>,
