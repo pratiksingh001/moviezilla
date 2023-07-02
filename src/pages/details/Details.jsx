d481235b733d2723e77c15ddce36fcf2b0ea3f7f
Pratik Singh <singhpratik0001@gmail.com>
2023-07-02 16:31:50 +0530

    Details page complete

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
 import "./style.scss"
+import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideosSection from './videoSection/VideosSection'
+import Recommendation from './carousels/Recommendation'
+import Similar from './carousels/Similar'
 
 const Details = () => {
+  const { mediaType, id } = useParams()
+  const {data, loading} = useFetch(`/${mediaType}/${id}/videos`)
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
+
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id}/>
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
   )
 }
 
--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react'
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
-import useFetch from '../../../hooks/useFetch'
-import Carousel from '../../../components/carousel/Carousel'
+import React, { useState } from "react";
 
+import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
+import useFetch from "../../../hooks/useFetch";
 
 const Popular = () => {
+    const [endpoint, setEndpoint] = useState("movie");
 
-    const [endPoint, setEndPoint] = useState('movie')
-    const { data, loading } = useFetch(`/${endPoint}/popular`)
-    
+    const { data, loading } = useFetch(`/${endpoint}/popular`);
+
     const onTabChange = (tab) => {
-        setEndPoint(tab === 'Movies' ? 'movie' : 'tv');
-    }
-  
-    return (
-    <div className='carouselSection'>
-        <ContentWrapper>
-            <span className="carouselTitle">Popular</span>
-            <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={ onTabChange } />
-        </ContentWrapper>
-        <Carousel data={data?.results} loading={loading} />
-    </div>
-    )
-}
+        setEndpoint(tab === "Movies" ? "movie" : "tv");
+    };
 
-export default Popular
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">What's Popular</span>
+                <SwitchTabs
+                    data={["Movies", "TV Shows"]}
+                    onTabChange={onTabChange}
+                />
+            </ContentWrapper>
+            <Carousel
+                data={data?.results}
+                loading={loading}
+                endpoint={endpoint}
+            />
+        </div>
+    );
+};
+
+export default Popular;
